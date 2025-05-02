91a2127e17965e8f60470197c3465639b8024e4b
mr-X-force <frediezra0@gmail.com>
2025-05-02 13:20:51 +0300

    Add files via upload

---
+++ plugins/fredi.js
@@ -0,0 +1,98 @@
+const util = require('util');
+const fs = require('fs-extra');
+const { ezra } = require(__dirname + "/../fredi/ezra");
+const { format } = require(__dirname + "/../fredi/mesfonctions");
+const os = require("os");
+const moment = require("moment-timezone");
+const s = require(__dirname + "/../set");
+const more = String.fromCharCode(8206);
+const readmore = more.repeat(4001);
+
+zokou({ nomCom: "menu", categorie: "Menu" }, async (dest, zk, commandeOptions) => {
+    let { ms, repondre, prefixe, nomAuteurMessage, mybotpic } = commandeOptions;
+    let { cm } = require(__dirname + "/../fredi/ezra");
+    let coms = {};
+    let mode = "public";
+
+    if ((s.MODE).toLowerCase() !== "yes") {
+        mode = "private";
+    }
+
+    cm.map((com) => {
+        if (!coms[com.categorie]) {
+            coms[com.categorie] = [];
+        }
+        coms[com.categorie].push(com.nomCom);
+    });
+
+    moment.tz.setDefault('Etc/GMT');
+    const temps = moment().format('HH:mm:ss');
+    const date = moment().format('DD/MM/YYYY');
+
+    let infoMsg = `
+╭━═「 *${s.BOT}* 」═━❂
+┃⊛╭────••••────➻
+┃⊛│◆ 𝙾𝚠𝚗𝚎𝚛 : ${s.OWNER_NAME}
+┃⊛│◆ 𝙿𝚛𝚎𝚏𝚒𝚡 : [ ${s.PREFIXE} ]
+┃⊛│◆ 𝙼𝚘𝚍𝚎 : *${mode}*
+┃⊛│◆ 𝚁𝚊𝚖  : 𝟴/𝟭𝟯𝟮 𝗚𝗕
+┃⊛│◆ 𝙳𝚊𝚝𝚎  : *${date}*
+┃⊛│◆ 𝙿𝚕𝚊𝚝𝚏𝚘𝚛𝚖 : ${os.platform()}
+┃⊛│◆ 𝙲𝚛𝚎𝚊𝚝𝚘𝚛 : ғʀᴇᴅɪᴇ ᴛᴇᴄʜ
+┃⊛│◆ 𝙲𝚘𝚖𝚖𝚊𝚗𝚍𝚜 : ${cm.length}
+┃⊛│◆ 𝚃𝚑𝚎𝚖𝚎 : FRED
+┃⊛└────••••────➻
+╰─━━━━══──══━━━❂\n${readmore}
+`;
+
+    let menuMsg = `𝙻𝚞𝚌𝚔𝚢 𝙼𝚍 𝙲𝚖𝚍`;
+    
+    for (const cat in coms) {
+        menuMsg += `
+❁━━〔 *${cat}* 〕━━❁
+╭━━══••══━━••⊷
+║◆┊ `;
+        for (const cmd of coms[cat]) {
+            menuMsg += `          
+║◆┊ ${s.PREFIXE}  *${cmd}*`;    
+        }
+        menuMsg += `
+║◆┊
+╰─━━═••═━━••⊷`;
+    }
+    
+    menuMsg += `
+> Made By ғʀᴇᴅɪᴇ ᴛᴇᴄʜ\n`;
+        
+        // Send message with an image
+        await zk.sendMessage(
+            from,
+            {
+                image: { url: 'https://files.catbox.moe/2ia6od.jpeg' }, // Image URL
+                caption: envSettings,
+                contextInfo: {
+                    mentionedJid: [m.sender],
+                    forwardingScore: 999,
+                    isForwarded: true,
+                    forwardedNewsletterMessageInfo: {
+                        newsletterJid: '120363313124070136@newsletter',
+                        newsletterName: "frediEzra",
+                        serverMessageId: 143
+                    }
+                }
+            },
+            { quoted: mek }
+        );
+
+        // Send an audio file
+        await conn.sendMessage(from, {
+            audio: { url: 'https://github.com/Fred1e/fredi/raw/refs/heads/main/autovoice/menu.mp3' }, // Audio URL
+            mimetype: 'audio/mp4',
+            ptt: true
+        }, { quoted: mek });
+   
+     } catch (error) {
+        console.error("Menu error: ", error);
+        repondre("🥵🥵 Menu error: " + error);
+    }
+});
